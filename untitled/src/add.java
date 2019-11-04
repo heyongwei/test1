dbe1b38a85de579e34594fa5c6bbd70fa0097184
EX_HEYW <yongwei.he@partner.midea.com.cn>
2019-11-04 17:42:13 +0800

    bbb

--- untitled/src/add.java
+++ untitled/src/add.java
@@ -2,4 +2,7 @@ public class add {
     public void aa(){
         System.out.printf("11");
     }
+    public void dd(){
+        System.out.printf("dd");
+    }
 }
--- untitled/src/update.java
+++ untitled/src/update.java
@@ -1,5 +1,8 @@
 public class update {
-    public void aa (){
+    public void aa(){
         System.out.printf("11");
+    }
+    public void cc(){
+        System.out.printf("cc");
     }
 }
